37d0bc5e381119fb22c1ac4281c9da23626c3a2e
soliddante <solidanddaily@gmail.com>
2022-08-04 15:37:40 +0430

    ok

--- resources/views/components/layouts/menu.blade.php
+++ resources/views/components/layouts/menu.blade.php
@@ -1,4 +1,4 @@
-<div class="gls_menu  hidden   fixed top-[64px] right-0 w-screen  h-screen bg-black bg-opacity-40 z-40">
+<div class="gls_menu  hidden     fixed top-[64px] right-0 w-screen  h-screen bg-black bg-opacity-40 z-40">
     <div id="menu" class="jsc_right_menu md:w-[360px] w-full absolute bg-white h-screen right-0 top-0">
         <x-layouts.menu_inner></x-layouts.menu_inner>
     </div>
