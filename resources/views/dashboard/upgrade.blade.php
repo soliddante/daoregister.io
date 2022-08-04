b96c0383c6f6e5d9c29fc78288216747ab31fce2
soliddante <solidanddaily@gmail.com>
2022-08-04 16:40:10 +0430

    ok

--- resources/views/dashboard/upgrade.blade.php
+++ resources/views/dashboard/upgrade.blade.php
@@ -116,6 +116,27 @@
                 </div>
 
             </div>
+        @else
+            <div class="grid grid-cols-5 gap-4">
+                <!-- This example requires Tailwind CSS v2.0+ -->
+                <div class="rounded-md bg-green-50 p-4">
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <!-- Heroicon name: solid/check-circle -->
+                            <ion-icon name="diamond-outline" class="h-5 w-5 text-green-400"></ion-icon>
+                        </div>
+                        <div class="ml-3">
+                            <p class="text-sm font-medium text-green-800">Your subscription is <strong>Premium</strong> and you can sign or create a new contract</p>
+                        </div>
+                        <div class="ml-auto pl-3">
+                            <div class="-mx-1.5 -my-1.5">
+                                <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Watch certificate </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         @endif
 
     </section>
